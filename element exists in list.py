6515348204b9c3c1ac2dfee3abcfc0e27d706ba1
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2022-12-02 12:24:00 +0100

    Add files via upload

---
+++ element exists in list.py
@@ -0,0 +1,21 @@
+"""
+Check if element exists in list in Python
+There are two methods that I could think of!
+"""
+
+# array = [9, 4, 8, 1, 0, 3]
+# element = 2
+#
+# if element in array:
+#     print(element, "exists")
+# else:
+#     print(element, "Doesn't exist")
+
+
+array = [9, 4, 8, 1, 0, 3]
+for i in array:
+    if i == 2:
+        print("Exists")
+
+
+
